f3a522432e3cb9f4b725204ce571c2ee4896d09d
Apala Ghosh <Apala_Ghosh@Dellteam.com>
2021-09-05 22:49:58 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Signup from "./pages/signup/Signup";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+// import { auth } from "./auth/Auth";
+// import { onAuthStateChanged } from "firebase/auth";
+// import StateContext from "../../Context/Context";
 
 function App() {
+  // onAuthStateChanged(auth, (user) => {
+  //   if (user) {
+  //     // const uid = user.uid;
+  //     handleuserId(user.uid);
+  //     handleuserName(user.displayName ?? user.email);
+  //     history.push("/Home");
+  //     console.log(uid, "SignIn");
+  //   } else {
+  //     handleEmail("");
+  //     handlePassword("");
+  //     handleuserId(null);
+  //     history.push("/Login");
+  //     console.log("SignOut");
+  //   }
+  // });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/Login">
+          <Login />
+        </Route>
+        <Route exact path="/Signup">
+          <Signup />
+        </Route>
+        <Route exact path="/Home">
+          <Home />
+        </Route>
+        <Route exact path="/Profile/:Username">
+          <Profile />
+        </Route>
+        <Route path="/">
+          <Redirect to="/Login" />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
